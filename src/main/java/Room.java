d61151d3f1e9999b00cec3553b862df1c4b11441
marbin701 <marvin07012002@gmail.com>
2026-05-05 11:13:45 -0400

    for the class Room different private fields has been created.

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,3 +1,8 @@
 public class Room {
+    private int numberOfBeds;
+    private double price;
+    private final boolean isOccupied = true;
+    private boolean isDirty;
+    private boolean isAvailable;
 
 }
