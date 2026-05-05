b32ece79f7ee86c408cec6a1fb69c05276fd1d36
marbin701 <marvin07012002@gmail.com>
2026-05-05 11:25:19 -0400

    getters and setters had been added to reservation

--- src/main/java/Reservation.java
+++ src/main/java/Reservation.java
@@ -5,4 +5,35 @@ public class Reservation {
     private boolean isWeekend;
     private double reservationTotal;
 
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getNumbersOfNights() {
+        return numbersOfNights;
+    }
+
+    public void setNumbersOfNights(int numbersOfNights) {
+        this.numbersOfNights = numbersOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getReservationTotal() {
+        return reservationTotal;
+    }
 }
