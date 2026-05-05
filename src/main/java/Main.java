ba87b8c24817d90e1e1e95a224f8b95ef7e8669a
marbin701 <marvin07012002@gmail.com>
2026-05-05 11:04:56 -0400

    Different classes have been created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        Room room1 = new Room();
+        Reservation reservation = new Reservation();
+        Employee employee = new Employee();
+
+
+    }
+
 }
